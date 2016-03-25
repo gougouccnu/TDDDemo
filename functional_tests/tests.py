23d669719bb662c79811cabc5e0b1ae5f22574e2
gougouccnu <lishaoweiccnu@163.com>
2016-03-25 23:10:01 +0800

    make functional_tests an app, use LiverServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys  
-import unittest
+#import unittest
+from django.test import LiveServerTestCase
 
-class NewVistorTest(unittest.TestCase):
+class NewVistorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -18,7 +19,7 @@ class NewVistorTest(unittest.TestCase):
 
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
