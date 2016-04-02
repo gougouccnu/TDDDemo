efa66373049e9a18c69264d957d836b8d684d4dd
gougouccnu <lishaoweiccnu@163.com>
2016-04-02 09:04:08 +0800

    在2个表单中添加URL，视图和测试有代码移动，添加了一个新URL

--- lists/views.py
+++ lists/views.py
@@ -7,15 +7,13 @@ def home_page(request):
 	#return HttpResponse(b'<html><title>To-Do lists</title></html>')
 	#if request.method == 'POST':
 	#	return HttpResponse(request.POST['item_text'])
-	if request.method == 'POST':
-		#new_item_text = request.POST['item_text']
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
-
-	items = Item.objects.all()
-	return render(request, 'home.html', {'items': items})
+	return render(request, 'home.html')
 
 def view_list(request):
 	
 	items = Item.objects.all()
 	return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
