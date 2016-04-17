35b6dae58bce5fdae6b54063f3109365553e7a5b
gougouccnu <lishaoweiccnu@163.com>
2016-04-17 09:45:59 +0800

    add functional test of list item validation test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,8 +7,23 @@ from .base import FunctionTest
 
 class ItemValidationTest(FunctionTest):
 
-	
 	def test_cannot_add_empty_list_items(self):
-		self.fail('write me!')
+		
+		self.browser.get(self.server_url)
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error, "You can't have a empty list item")
+
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+		self.check_for_row_in_list_table('1: Buy milk')
+
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+		error = self.browser.find_css_selector('.has-error')
+		self.assertEqual(error, "You can't have a empty list item")
+
+		self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+		self.check_for_row_in_list_table('1: Buy milk')
+		self.check_for_row_in_list_table('2: Make tea')
 		
