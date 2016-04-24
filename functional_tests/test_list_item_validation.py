5b336e680ad4271b881e56ef172858680ab1e5a1
gougouccnu <lishaoweiccnu@163.com>
2016-04-24 09:48:25 +0800

    ajust new list to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,20 +10,40 @@ class ItemValidationTest(FunctionTest):
 	def test_cannot_add_empty_list_items(self):
 		
 		self.browser.get(self.server_url)
+		# inputbox = self.browser.find_element_by_id('id_new_item')
+		# inputbox.send_keys('test not add empty Buy milk')
+		# inputbox.send_keys(Keys.ENTER)
+
+		# self.browser.implicitly_wait(3)
+
+		# # inputbox = self.browser.find_element_by_id('id_new_item')
+		# # inputbox.send_keys('Use peacock feathers to make a fly')
+		# # inputbox.send_keys(Keys.ENTER)
+
+		# self.check_for_row_in_list_table('1: Buy milk')
+		# self.browser.implicitly_wait(3)
+
 		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		self.browser.implicitly_wait(3)
 
 		error = self.browser.find_element_by_css_selector('.has-error')
-		self.assertEqual(error, "You can't have a empty list item")
+		self.browser.implicitly_wait(3)
 
-		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-		self.check_for_row_in_list_table('1: Buy milk')
+		self.assertEqual(error.text, "You can't have an empty list item")
+		self.browser.implicitly_wait(10)
 
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('after blank Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1:after blank Buy milk')
 
-		error = self.browser.find_css_selector('.has-error')
-		self.assertEqual(error, "You can't have a empty list item")
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys(Keys.ENTER)
+
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You can't have an empty list item")
 
 		self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-		self.check_for_row_in_list_table('1: Buy milk')
-		self.check_for_row_in_list_table('2: Make tea')
+		self.check_for_row_in_list_table('1:after blank Buy milk')
+		self.check_for_row_in_list_table('2:Make tea')
 		
